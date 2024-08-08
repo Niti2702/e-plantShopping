0e4ff41481b29f65734dff7e3b9a890911026ad8
Niti2702 <nitid2207@gmail.com>
2024-08-08 15:28:18 -0400

    update

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,29 +9,33 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+ return cart.reduce((total,item)=> total + (item.quantity = parseInt(item.cost.replace('$', ''), 0)), 0)
   };
 
   const handleContinueShopping = (e) => {
-   
+   onContinueShopping(e);
   };
 
-
-
-  const handleIncrement = (item) => {
+const handleIncrement = (item) => {
+dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1}));
   };
 
   const handleDecrement = (item) => {
-   
+   if (item.quantity - 1 === 0) {
+    handleRemove(item);
+   } else {
+    dispatch(updateQuantity({ name: item.name,quantity: item.quantity - 1 }));
+   }
   };
 
   const handleRemove = (item) => {
-  };
+dispatch(removeItem({name: item.name,quantity: item.quantity}));  
+};
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+  return item.quantity * parseInt(item.cost.replace('$', ''),0);
   };
-
   return (
     <div className="cart-container">
       <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,14 +7,29 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const { name, image, cost } = action.payload;
+        const existingItem = state.items.find(item => item.name === name);
+        if (existingItem) {
+          existingItem.quantity++;
+      state.totalQuantity += existingItem.quantity;
+      } else {
+          state.items.push({ name, image, cost, quantity: 1 });
+    state.totalQuantity++;
+        }
     },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
-
-    
-    },
+    removeItem: (state,action) => {
+        const {name,quantity} = action.payload;
+        state.items = state.items.filter(item => item.name !== name);
+    state.totalQuantity -= quantity;
+   },
+   updateQuantity: (state,action) => {
+   const { name, quantity } = action.payload;
+   const itemToUpdate = state.items.find(item => item.name === name);
+   if (itemToUpdate) {
+    state.totalQuantity += quantity - itemToUpdate.quantity;
+     itemToUpdate.quantity = quantity;
+   }
+},
   },
 });
 
